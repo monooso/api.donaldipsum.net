c48bcbc0298efe1428cca458a9397715245a3660
Stephen Lewis <stephen@experiencehq.net>
2016-06-17 23:34:47 +0100

    Initial commit.

---
+++ app.js
@@ -0,0 +1,81 @@
+'use strict';
+
+var app;
+var generator;
+var seedText;
+
+app = require('express')();
+seedText = require('./lib/transcriptLoader');
+generator = require('./lib/generator')(seedText);
+
+/*
+https://api.donaldipsum.net/v1.0.0/words?count=10
+https://api.donaldipsum.net/v1.0.0/sentences?count=3
+https://api.donaldipsum.net/v1.0.0/paragraphs?count=3
+
+{
+   "success": true,
+   "content": ["Trust me."]
+}
+*/
+
+/**
+ * Builds a "success" response object.
+ * @param   {string} content
+ * @returns {boolean}
+ */
+function buildSuccessfulResponse(content) {
+    return {
+        success: true,
+        content: content,
+    };
+}
+
+/**
+ * Generates an arbitrary number of paragraphs.
+ */
+app.get('/:version/paragraphs', function (req, res) {
+    var content;
+    var count;
+
+    // Default to 3 paragraphs.
+    count = req.query.count || 3;
+    content = generator.generateParagraphs(count);
+
+    res.status(200).json(buildSuccessfulResponse(content));
+});
+
+/**
+ * Generates an arbitrary number of sentences.
+ */
+app.get('/:version/sentences', function (req, res) {
+    var content;
+    var count;
+
+    // Default to 3 sentences.
+    count = req.query.count || 3;
+    content = generator.generateSentences(count);
+
+    res.status(200).json(buildSuccessfulResponse(content));
+});
+
+/**
+ * Generates an arbitrary number of words.
+ */
+app.get('/:version/words', function (req, res) {
+    var content;
+    var count;
+
+    // Default to 10 words.
+    count = req.query.count || 10;
+    content = [generator.generateWords(count)];
+
+    res.status(200).json(buildSuccessfulResponse(content));
+});
+
+/**
+ * Starts the server.
+ */
+app.listen(3000, function () {
+    console.log('Application started.');
+});
---
+++ test/generator.js
@@ -0,0 +1,49 @@
+'use strict';
+
+let expect;
+let generator;
+
+expect = require('chai').expect;
+generator = require('../lib/generator');
+
+describe('generator', function () {
+    let seed;
+    let subject;
+
+    beforeEach(function () {
+        seed = 'We were somewhere around Barstow, on the edge of the desert, when the drugs began to take hold';
+        subject = generator(seed);
+    });
+
+    describe('generateParagraphs', function () {
+        it('should generate a string containing a specific number of paragraphs', function () {
+            expect(subject.generateParagraphs(5).length).to.equal(5);
+        });
+    });
+
+    describe('generateSentences', function () {
+        it('should generate a string containing a specific number of sentences', function () {
+            expect(subject.generateSentences(5).length).to.equal(5);
+        });
+    });
+
+    describe('generateWords', function () {
+        it('should generate a string containing a specific number of words', function () {
+            expect(subject.generateWords(5).split(' ').length).to.equal(5);
+        });
+
+        it('should generate a string containing an awful lot of words', function () {
+            expect(subject.generateWords(150).split(' ').length).to.equal(150);
+        });
+
+        it('should strip double-quotes from the string', function () {
+            let output;
+            let subject;
+
+            subject = generator('Remove "these quotes"');
+            output = subject.generateWords(20);
+
+            expect(output.indexOf('"')).to.equal(-1);
+        });
+    });
+});
